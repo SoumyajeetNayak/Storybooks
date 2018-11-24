9096cd3349b639a24260d4c29eb95b5d101247de
Soumyajeet <soumyajeet08@gmail.com>
2018-11-24 13:11:36 +0530

    adding google authentication

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport')(passport)
+
+const auth = require('./Routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It is working');
 });
+
+app.use('/auth', auth);
 
 const PORT =    process.env.PORT || 5000;
 
