3e09ca97f3bf9c7c15ee099b08bb68d2fce6658e
Soumyajeet <soumyajeet08@gmail.com>
2018-11-24 18:48:30 +0530

    adding more features

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const keys = require('./config/keys');
+
+
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+//Load user model
+require('./Model/user');
 
 require('./config/passport')(passport)
 
+const index = require('./Routes/index');
 const auth = require('./Routes/auth');
+
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('connected to mongo DB');
+}).catch(err => console.log(err.message));
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('It is working');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Set Globar Vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 });
 
+
+app.use('/', index);
 app.use('/auth', auth);
+
+
+
+
 
 const PORT =    process.env.PORT || 5000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://soumyajeet:Lipun@100214@ds135757.mlab.com:35757/storybooks-soumyajeet-dev',
+    mongoURI: 'mongodb://soumyajeet:Lipun#100214@ds135757.mlab.com:35757/storybooks-soumyajeet-dev',
     googleClientID: '815746193568-kabqmrbbnp2occe6lavop0qifk7ojgq0.apps.googleusercontent.com',
     googleClientSecret: 'VHN-YV-8Gorob6yUxISvfBt5'
 }
